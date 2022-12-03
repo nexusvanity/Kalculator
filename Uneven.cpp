b411c4d0a382488b37c4a99776c9279f5fec7029
BloomBerry <111573879+BloomBerry-Berg@users.noreply.github.com>
2022-12-03 08:48:49 +0000

    Source code
    Uneven's Source Code, lol

---
+++ Uneven.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int x;
+cout << "Hello! Enter your number, We'll check it."<<endl;
+cin>>x;
+if (x % 2 == 0 )
+cout << "This number is even.";
+else
+cout << "This number's uneven.";
+return 0;}
